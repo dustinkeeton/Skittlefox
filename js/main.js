1ce7c9cb2166d5ff2a4018fd83636dad4dcfd549
Dustin Keeton <dustin.keeton49@gmail.com>
2013-03-29 18:51:51 -0400

    Changed animation function. Fixed icon z-index.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // AUTHOR: Dustin Keeton
 $(document).ready(function() {
 	
-	var extended = false;
 	var $links = $('#links');
+	$links.addClass('invisible');
 
 	// set colorbox.js settings
 	$.colorbox.settings.height= "100%";
@@ -10,22 +10,10 @@ $(document).ready(function() {
 	$.colorbox.settings.maxWidth=1200;
 
 	// Expand/collapse menu 
-	$('#logo').click(function() {
-		
+	$('#logo').click(function() {	
 		// expand/collapse clicked menu
-		animateLinks();
-		if (parseInt($('#menuContainer').css('left'),10) == -700) {
-			$('#menuContainer').animate({left: -100}, {duration: 1000, easing: 'easeOutBack'});
-			$('#about').animate({left: 0}, {duration: 1000, easing: 'easeOutBack'});
-			$('#copyright').animate({left: 200}, {duration: 1000, easing: 'easeOutBack'});
-		}
-		else {
-			$('#menuContainer').animate({left: -700}, {duration: 1000, easing: 'easeInBack'});
-			$('#about').animate({left: -999}, {duration: 1000, easing: 'easeInBack'});
-			$('#copyright').animate({left: -999}, {duration: 1000, easing: 'easeInBack'});
-		}
+		animateStuff();
 	});
-
 
 	// gallery lightbox
 	$('.gallery').click(function() {
@@ -35,22 +23,35 @@ $(document).ready(function() {
 		});
 	});
 
-	// animate links from behind 
-	function animateLinks(){
-		if (!extended) {
+	// animate stuff
+	function animateStuff(){
+		if (parseInt($('#menuContainer').css('left'),10) == -700) {
+			//Menu pieces
+			$('#menuContainer').animate({left: -100}, {duration: 1000, easing: 'easeOutBack'});
+			$('#about').animate({left: 0}, {duration: 1000, easing: 'easeOutBack'});
+			$('#copyright').animate({left: 200}, {duration: 1000, easing: 'easeOutBack'});
+			//links
 			$links.removeClass('invisible');
 			$('#wordpress').animate({top: -50}, {duration: 500});
 			$('#deviantart').animate({left: 80, top: 10}, {duration: 500});
 			$('#twitter').animate({left: 100, top: 50}, {duration: 500});
-			$('#cghub').animate({left: 45, top: -20}, {duration: 500});
-			extended = true;
+			$('#cghub').animate({left: 45, top: -20}, {duration: 500, complete: function(){
+				$links.css('z-index', '+1');
+			}});	
 		}
 		else {
+			//Menu pieces
+			$('#menuContainer').animate({left: -700}, {duration: 1000, easing: 'easeInBack'});
+			$('#about').animate({left: -999}, {duration: 1000, easing: 'easeInBack'});
+			$('#copyright').animate({left: -999}, {duration: 1000, easing: 'easeInBack'});
+			//links
+			$links.css('z-index', '-1');
 			$('#wordpress').animate({top: 0}, {duration: 500});
 			$('#deviantart').animate({left: 0, top: 0}, {duration: 500});
 			$('#twitter').animate({left: 0, top: 0}, {duration: 500});
-			$('#cghub').animate({left: 0, top: 0}, {duration: 500});
-			extended = false;
+			$('#cghub').animate({left: 0, top: 0}, {duration: 500, complete: function(){
+				$links.addClass('invisible');
+			}});	
 		}
 	} 
 
