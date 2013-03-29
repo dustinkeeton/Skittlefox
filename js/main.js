2d0152c451975c1fa117be9fbf0b5839e62997ca
Dustin Keeton <dustin.keeton49@gmail.com>
2013-03-28 20:11:25 -0400

    Altered easing. Changed link locations. Added animation to links.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // AUTHOR: Dustin Keeton
 $(document).ready(function() {
 	
+	var extended = false;
+	var $links = $('#links');
 	// set colorbox.js settings
 	// $.colorbox.settings.height= "100%";
 	// $.colorbox.settings.maxHeight=850;
@@ -13,9 +15,15 @@ $(document).ready(function() {
 
 		if (parseInt($('#menuContainer').css('left'),10) == -700) {
 			$('#menuContainer').animate({left: -100}, {duration: 1000, easing: 'easeOutBack'});
+			$('#about').animate({left: 0}, {duration: 1000, easing: 'easeOutBack'});
+			$('#copyright').animate({left: 200}, {duration: 1000, easing: 'easeOutBack'});
+			animateLinks();
 		}
 		else {
-			$('#menuContainer').animate({left: -700}, {duration: 1000, easing: 'easeOutBack'});
+			animateLinks();
+			$('#menuContainer').animate({left: -700}, {duration: 1000, easing: 'easeInBack'});
+			$('#about').animate({left: -999}, {duration: 1000, easing: 'easeInBack'});
+			$('#copyright').animate({left: -999}, {duration: 1000, easing: 'easeInBack'});
 		}
 	});
 
@@ -27,4 +35,24 @@ $(document).ready(function() {
 			rel: 'gallery'
 		});
 	});
+
+	function animateLinks(){
+		if (!extended) {
+			$links.removeClass('invisible');
+			$('#wordpress').animate({top: -50}, {duration: 500});
+			$('#deviantart').animate({left: 80, top: 10}, {duration: 500});
+			$('#twitter').animate({left: 100, top: 50}, {duration: 500});
+			$('#cghub').animate({left: 45, top: -20}, {duration: 500});
+			extended = true;
+		}
+		else {
+			$('#wordpress').animate({top: 0}, {duration: 500});
+			$('#deviantart').animate({left: 0, top: 0}, {duration: 500});
+			$('#twitter').animate({left: 0, top: 0}, {duration: 500});
+			$('#cghub').animate({left: 0, top: 0}, {duration: 500});
+			extended = false;
+		}
+	} 
+
+
 });
