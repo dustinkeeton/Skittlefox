29f0ed9edbf8b514f773827520b9ed611f91239f
Dustin Keeton <dustin.keeton49@gmail.com>
2013-04-08 02:31:58 -0400

    More responsive layout changes. Working on copy positioning.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // AUTHOR: Dustin Keeton
 $(document).ready(function() {
 	var $windowWidth = $(window).width();
+	// alert($windowWidth);
 	var $left = '-58.59375%';
 	var $links = $('#links');
 	$links.addClass('invisible');
@@ -18,6 +19,12 @@ $(document).ready(function() {
   		}
 	});
 
+	//orientation change
+	$(window).bind('orienationchange', function(){
+		animateStuff();
+	});
+
+
 	// Expand/collapse menu 
 	$('#logo').click(function() {	
 		// expand/collapse clicked menu
@@ -33,29 +40,52 @@ $(document).ready(function() {
 	});
 
 	// animate stuff
+
 	function animateStuff(){
+		//checking to see if menu is already expanded
 		if (parseInt($('#menuContainer').css('left')) < 0) {
-			//Menu pieces
-			$('#menuContainer').animate({left: '0%'}, {duration: 1000, easing: 'easeOutBack'});
-			$('#about').animate({left: 0}, {duration: 1000, easing: 'easeOutBack'});
-			//links
-			$links.removeClass('invisible');
-			$('#wordpress').animate({top: -55, left: -15}, {duration: 500});
-			$('#cghub').animate({top: -40, left: 26}, {duration: 500, complete: function(){
-				$links.css('z-index', '+1');
-			}});
-			$('#tumblr').animate({top: -15, left: 65}, {duration: 500});
-			$('#deviantart').animate({top: 20, left: 95}, {duration: 500});
-			$('#twitter').animate({top: 64, left: 105}, {duration: 500});
-				
+			//menu is not expaned - next check for iphone screen
+			if($windowWidth < 400){
+				//Menu pieces
+				$('#menuContainer').animate({left: '0%'}, {duration: 1000, easing: 'easeOutBack'});
+				$('#about').animate({left: 0}, {duration: 1000, easing: 'easeOutBack'});
+				//links
+				$links.removeClass('invisible');
+				$links.animate({top: 0, left: +90}, {duration: 500, complete: function(){
+					$links.css('z-index', '+1');
+					$('#cghub').animate({top: +40}, {duration: 500});
+					$('#tumblr').animate({top: +80}, {duration: 500});
+					$('#deviantart').animate({top: +120}, {duration: 500});
+					$('#twitter').animate({top: +160}, {duration: 500});
+				}});
+			}
+			//not iphone - cool animation proceeds!
+			else {
+				//Menu pieces
+				$('#menuContainer').animate({left: '0%'}, {duration: 1000, easing: 'easeOutBack'});
+				$('#about').animate({left: 0}, {duration: 1000, easing: 'easeOutBack'});
+				//links
+				$links.removeClass('invisible');
+				$('#wordpress').animate({top: -55, left: -15}, {duration: 500});
+				$('#cghub').animate({top: -40, left: 26}, {duration: 500, complete: function(){
+					$links.css('z-index', '+1');
+				}});
+				$('#tumblr').animate({top: -15, left: 65}, {duration: 500});
+				$('#deviantart').animate({top: 20, left: 95}, {duration: 500});
+				$('#twitter').animate({top: 64, left: 105}, {duration: 500});
+			}	
 		}
 		else {
 			//Animates back to starting position based on screen size 
 			if($windowWidth >= 1900) {
 				$left = '-110%';
+
+			}
+			else if ($windowWidth >= 1500) {
+				$left = '-80%';
 			}
 			else if ($windowWidth >= 1000) {
-				$left = '-70%';
+				$left = '-65%';
 			}
 			//Menu pieces
 			$('#menuContainer').animate({left: $left}, {duration: 1000, easing: 'easeInBack'});
