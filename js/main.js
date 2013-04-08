e06c11b03e1f1a38b06d35a9c1e74ef40c05546d
Dustin Keeton <dustin.keeton49@gmail.com>
2013-04-08 05:17:33 -0400

    responsive copyright. Next step is responsive/@media link animations.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // AUTHOR: Dustin Keeton
 $(document).ready(function() {
 	var $windowWidth = $(window).width();
-	alert($windowWidth);
+	// alert($windowWidth);
 	var $left = '-58.59375%';
 	var $links = $('#links');
 	
@@ -80,7 +80,7 @@ $(document).ready(function() {
 		if($windowWidth < 500){
 			//Menu pieces
 			$('#menuContainer').animate({left: '0%'}, {duration: 1000, easing: 'easeOutBack'});
-			$('#about').animate({left: 0}, {duration: 1000, easing: 'easeOutBack'});
+			$('#info').animate({left: 0}, {duration: 1000, easing: 'easeOutBack'});
 			//links
 			$links.removeClass('invisible');
 			$links.animate({top: 0, left: +90}, {duration: 500, complete: function(){
@@ -96,7 +96,7 @@ $(document).ready(function() {
 		else {
 			//Menu pieces
 			$('#menuContainer').animate({left: '0%'}, {duration: 1000, easing: 'easeOutBack'});
-			$('#about').animate({left: 0}, {duration: 1000, easing: 'easeOutBack'});
+			$('#info').animate({left: 0}, {duration: 1000, easing: 'easeOutBack'});
 			//links
 			$links.removeClass('invisible');
 			$('#wordpress').animate({top: -55, left: -15}, {duration: 500});
@@ -126,7 +126,7 @@ $(document).ready(function() {
 		}
 		//Menu pieces
 		$('#menuContainer').animate({left: $left}, {duration: 1000, easing: 'easeInBack'});
-		$('#about').animate({left: -999}, {duration: 1000, easing: 'easeInBack'});
+		$('#info').animate({left: -999}, {duration: 1000, easing: 'easeInBack'});
 		//links
 		$links.css('z-index', '-1');
 		$('#wordpress').animate({left: 0, top: 0}, {duration: 500});
